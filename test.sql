d622249a4c1fcfba9b75190878b3a8100479b0b2
Noreh AD <akinoreh@gmail.com>
2022-06-13 16:45:11 +0300

    test.sql

---
+++ test.sql
@@ -0,0 +1,57 @@
+
+-- #region ==================== URL
+
+(34226, '_transient_wpseo_site_information', '...;s:26:\"http://111.22.333.44/~user\";...', 'no'),
+
+s:125:\"<img src=\"http://111.22.333.44/~user/wp-content/uploads/2021/04/payments-methods.png\" alt=\"payments\" width=\"222\" height=\"23\">\";
+
+(1, 'siteurl', 'http://111.22.333.44/~user', 'yes'),
+
+(2637, 350, '_wpb_shortcodes_custom_css', '.vc_custom_1647412105384{background-image: url(http://111.22.333.44/~user/wp-content/uploads/2022/03/info-box-bg.jpg?id=371) !important;background-position: center !important;background-repeat: no-repeat !important;background-size: cover !important;}'),
+
+-- #endregion
+
+
+-- #region ==================== ESCAPED & ENCODED~ URL
+
+('45.178.189.87', 1321054774, '', 'http://111.22.333.44/~user/wp-login.php', '', '{\"1\":\"Mozilla\\/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit\\/537.36 (KHTML, like Gecko) Chrome\\/102.0.5005.63 Safari\\/537.36\",\"2\":\"http:\\/\\/111.22.333.44\\/~user\\/wp-login.php?redirect_to=http%3A%2F%2F111.22.333.44%2F%7Euser%2Fwp-admin%2F&reauth=1\"}', ...),
+
+-- #endregion
+
+
+-- #region ==================== ENCODED
+
+'[promo_banner image=\"1388\" hover=\"none\" horizontal_alignment=\"center\" vertical_alignment=\"middle\" text_alignment=\"center\" woodmart_css_id=\"627d0dae3fd81\" img_size=\"full\" hide_btn_tablet=\"no\" hide_btn_mobile=\"no\" increase_spaces=\"no\" link=\"url:http%3A%2F%2F111.22.333.44%2F~user%2Fabout\"]'
+
+-- #endregion
+
+
+-- #region ==================== URL W/O SCHEMA
+
+('45.178.189.87', 1321054774, ..., '0|0|0|0|111.22.333.44/~user/wp-login.php', 0, 0, 1),
+
+'@font-face {\n	font-weight: normal;\n	font-style: normal;\n	font-family: \"woodmart-font\";\n	src: url(\"//111.22.333.44/~user/wp-content/themes/woodmart/fonts/woodmart-font.woff2?v=6.1\") format(\"woff2\"),\n	url(\"//111.22.333.44/~user/wp-content/themes/woodmart/fonts/woodmart-font.woff?v=6.1\") format(\"woff\");'
+
+-- #endregion
+
+
+-- #region ==================== DOMAIN
+
+(1, 1546, 'WooCommerce', 'woocommerce@111.22.333.44', '', ...),
+
+(34222, '_transient_timeout_external_ip_address_111.22.333.44', '1654582406', 'no'),
+
+(34223, '_transient_external_ip_address_111.22.333.44', '111.22.333.44', 'no'),
+
+(1569, 240, 'from_header', 'WordPress <wordpress@111.22.333.44>'),
+
+-- #endregion
+
+
+-- #region ==================== PATH
+
+(1273, 'fs_active_plugins', 'O:8:\"stdClass\":3:{s:7:\"plugins\";a:1:{s:30:\"woo-permalink-manager/freemius\";O:8:\"stdClass\":4:{s:7:\"version\";s:5:\"2.4.2\";s:4:\"type\";s:6:\"plugin\";s:9:\"timestamp\";i:1617192961;s:11:\"plugin_path\";s:47:\"woo-permalink-manager/premmerce-url-manager.php\";}}s:7:\"abspath\";s:51:\"/home/johndoe/domains/johndoe.com/public_html/\";s:6:\"newest\";O:8:\"stdClass\":5:{s:11:\"plugin_path\";s:47:\"woo-permalink-manager/premmerce-url-manager.php\";s:8:\"sdk_path\";s:30:\"woo-permalink-manager/freemius\";s:7:\"version\";s:5:\"2.4.2\";s:13:\"in_activation\";b:0;s:9:\"timestamp\";i:1617192961;}}', 'yes'),
+
+(18, ..., 'attachment-image', '{\"width\":1200,\"height\":1200,\"url\":\"http://111.22.333.44/~user/wp-content/uploads/woocommerce-placeholder.png\",\"path\":\"/home/johndoe/domains/johndoe.com/public_html/wp-content/uploads/woocommerce-placeholder.png\",\"size\":\"full\",\"id\":15,\"alt\":\"\",\"pixels\":1440000,\"type\":\"image/png\"}', ...),
+
+-- #endregion
