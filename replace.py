0097a81f2f211e74fd11f7377765cb72d061cb42
Noreh AD <akinoreh@gmail.com>
2022-06-13 12:58:29 +0300

    replace domain & path

--- replace.py
+++ replace.py
@@ -106,6 +106,9 @@ input_dict["normal"]["dbl_escaped_fs"]  = input_url.replace("/", r"\\/")
 input_dict["normal"]["encoded"]         = urllib.parse.quote(input_url, safe="")
 input_dict["normal"]["encoded_tilde"]   = urllib.parse.quote(input_url, safe="").replace("~", "%7E")
 input_dict["normal"]["dbl_encoded"]     = urllib.parse.quote(urllib.parse.quote(input_url, safe=""), safe="")
+input_dict["normal"]["url_wo_schema"]   = re.sub("^https?://", "", input_url)
+input_dict["normal"]["domain"]          = urllib.parse.urlparse(input_url).netloc
+input_dict["normal"]["path"]            = "home/%username%/domains/%domain%/public_html"
 
 output_url = "http://example.com"
 output_dict = {
@@ -120,6 +123,9 @@ output_dict["normal"]["dbl_escaped_fs"]  = output_url.replace("/", r"\\/")
 output_dict["normal"]["encoded"]         = urllib.parse.quote(output_url, safe="")
 output_dict["normal"]["encoded_tilde"]   = urllib.parse.quote(output_url, safe="").replace("~", "%7E")
 output_dict["normal"]["dbl_encoded"]     = urllib.parse.quote(urllib.parse.quote(output_url, safe=""), safe="")
+output_dict["normal"]["url_wo_schema"]   = re.sub("https?://", "", output_url)
+output_dict["normal"]["domain"]          = urllib.parse.urlparse(output_url).netloc
+output_dict["normal"]["path"]            = "home/%username%/htdocs"
 
 content = replace_serialized(content, input_dict["serialized"]["url"], output_dict["serialized"]["url"])
 content = content.replace(input_dict["serialized"]["url"], output_dict["serialized"]["url"])
@@ -127,6 +133,9 @@ content = content.replace(input_dict["normal"]["dbl_escaped_fs"], output_dict["n
 content = content.replace(input_dict["normal"]["encoded"], output_dict["normal"]["encoded"])
 content = content.replace(input_dict["normal"]["encoded_tilde"], output_dict["normal"]["encoded_tilde"])
 content = content.replace(input_dict["normal"]["dbl_encoded"], output_dict["normal"]["dbl_encoded"])
+content = content.replace(input_dict["normal"]["url_wo_schema"], output_dict["normal"]["url_wo_schema"])
+content = content.replace(input_dict["normal"]["domain"], output_dict["normal"]["domain"])
+content = content.replace(input_dict["normal"]["path"], output_dict["normal"]["path"])
 
 output_file = open("output.sql", "w", encoding="utf-8")
 output_file.write(content)
